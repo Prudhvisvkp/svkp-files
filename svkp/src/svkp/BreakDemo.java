c1f2d679277ccbbc16898c558b4601e249b14449
Raju <Raju@LAPTOP-G60JVNO1>
2024-11-18 18:20:38 +0530

    commit 5

---
+++ svkp/src/svkp/BreakDemo.java
@@ -0,0 +1,18 @@
+package svkp;
+
+public class BreakDemo {
+
+
+public static void main(String[] args) {
+	for(int i=1;i<200;i++)
+	{
+		if(i>123)
+		{
+			break;
+		}
+		System.out.println(i);
+	}
+
+}
+
+}
