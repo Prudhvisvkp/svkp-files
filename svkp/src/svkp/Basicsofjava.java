4be29d9fb8e375c3962bd78ab6233645713e00d4
Raju <Raju@10.0.0.112>
2024-11-18 15:27:54 +0530

    commit one

---
+++ svkp/src/svkp/Basicsofjava.java
@@ -0,0 +1,17 @@
+package svkp;
+
+public class Basicsofjava {
+
+
+
+    public static void main(String[] args) {
+       int i=100;
+	   while(i>=1){
+		    
+		    System.out.println(i);
+		    i--;
+
+    }
+}
+}
+    
