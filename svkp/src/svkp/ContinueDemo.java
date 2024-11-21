83f4e4ab0a0c35b9137d6177e7361e23b6ede3ce
Raju <Raju@LAPTOP-G60JVNO1>
2024-11-21 21:16:32 +0530

    commit 8

---
+++ svkp/src/svkp/ContinueDemo.java
@@ -0,0 +1,18 @@
+package svkp;
+
+public class ContinueDemo {
+	
+	public static void main(String[] args) {
+		for(int i=1;i<15;i++)
+		{
+			if(i==5 || i==7)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
+	
+
+
