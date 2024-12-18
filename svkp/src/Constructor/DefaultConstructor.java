e8432d3c62674dd1f9efdac7d7065f0e7334f938
Raju <Raju@LAPTOP-G60JVNO1>
2024-12-18 08:16:35 +0530

    commit 16

---
+++ svkp/src/Constructor/DefaultConstructor.java
@@ -0,0 +1,32 @@
+package Constructor;
+
+class Myclass1
+{
+	String color;
+	int cost;
+	String brand;
+	
+	
+	//default constructor
+	Myclass1()
+	{
+		color="Green";
+		cost=2000000;
+		brand="TESLA";
+	}
+}
+
+
+public class DefaultConstructor {
+	
+	public static void main(String[] args) {
+		Myclass1 obj=new Myclass1();
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+		
+
+	}
+
+}
+
+
